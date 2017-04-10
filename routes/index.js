436a43df2b9bb910e1eca8f859d5972040500100
Corentin MOREAU <corentin.moreau2@gmail.com>
2017-04-10 16:16:03 +0200

    je commit des choses

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var socket = require('socket.io');
+
+var io = socket.listen(8080);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+function newBoard(){
+  var SIZE = 19;
+  var b = new Array();
+  for (var i = 0; i < SIZE; i++) {
+    b[i] = new Array();
+    for (var j = 0; j < SIZE; j++) {
+      b[i][j] = {stone : null};
+    }
+  }
+  return b;
+}
+
+var boardElement = document.querySelector(".tenuki-board");
+var game = new tenuki.Game(boardElement);
+game.setup();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var board = newBoard();
+  res.render('index', { board: board });
 });
 
 module.exports = router;
