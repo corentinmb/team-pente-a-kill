d0018c0db0e33e850431b60ee8533acec8d4cbd8
Godevin <vibertkevin@yahoo.fr>
2017-04-11 14:46:23 +0200

    Debut http request turn function

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Game = require('../models/gameModel.js');
 var game = null;
+var numTour = 0;
 
 
 /* GET home page. */
@@ -36,12 +37,30 @@ router.get('/connect/:groupName', function(req, res, next) {
 
 /* GET play */
 router.get('/play/:x/:y/:idJoueur', function(req, res, next) {
-  res.send(JSON.stringify(req.params))
+
+
 });
 
 /* GET turn */
 router.get('/turn/:idJoueur', function(req, res, next) {
-  res.send(JSON.stringify(req.params))
+  if(req.params.idJoueur == game.player2.idJoueur || req.params.idJoueur == game.player1.idJoueur){
+  numTour = numTour + 1;
+  res.json({"status" : 0,
+  			"tableau" : game.board,
+  			"nbTenaillesJ1" : 0,
+  			"nbTenaillesJ2" : 0,
+  			"dernierCoupX" : 0,
+  			"dernierCoupY" : 0,
+  			"prolongation" : false,
+  			"finPartie" : false,
+  			"detailFinPartie" : 'Fin partie',
+  			"numTour" : numTour,
+  			"code" : 200
+           })
+  }
+  else{
+  	res.sendStatus(401);
+  }
 });
 
 module.exports = router;
