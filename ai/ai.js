fdf36f05cdd07dbb5dda748c1754aca1871783bc
Corentin MOREAU <corentin.moreau2@gmail.com>
2017-04-13 00:40:16 +0200

    IA primaire + deplacements random

--- ai/ai.js
+++ ai/ai.js
@@ -2,10 +2,13 @@ var log = require('fancy-log');
 var request = require('request');
 
 var IAconfig = {
-    adresseAPI: '',
-    nomEquipe: '',
-    data: ''
+    adresseAPI: null,
+    nomEquipe: null,
+    dataConnect: null,
+    dataTurn: null
 };
+
+var finPartie = false;
 
 
 function init() {
@@ -18,38 +21,85 @@ function init() {
             console.log('Usage: node ai.js <AdresseAPI> <NomEquipe>');
             console.log('Exemple: node ai.js http://localhost:3000 Bordeaux');
         });
-
     }
-
 }
 
-function connect() {
+function connect(callback) {
     log.info("Connexion de l'IA...")
-    reqConnexion(success);
-}
-
-function success(b){
-  log.info("IA connectée avec succès !")
-  IAconfig.data = b;
-}
-
-function reqConnexion(callback) {
     request(IAconfig.adresseAPI + "/connect/" + IAconfig.nomEquipe, function(error, response, body) {
         if (response && response.statusCode == 200) {
-            callback(body);
+          log.info("IA connectée avec succès !")
+          IAconfig.dataConnect = JSON.parse(body);
+          callback(IAconfig.dataConnect);
         } else {
-            process.on('exit', (code) => {
-                log.error('Erreur de connexion à l\'API');
-            });
+          process.on('exit', (code) => {
+              log.error('Erreur de connexion à l\'API');
+          });
         }
 
     });
 }
 
+function printInConsole(b){
+  //console.log(b)
+}
+
+function play(x,y){
+  request(IAconfig.adresseAPI + "/play/" + x + "/" + y + "/" + IAconfig.dataConnect.idJoueur, function(error, response, body) {
+  });
+
+}
+
+function turn(callback){
+  request(IAconfig.adresseAPI + "/turn/" + IAconfig.dataConnect.idJoueur, function(error, response, body) {
+      if (response && response.statusCode == 200) {
+        IAconfig.dataTurn = JSON.parse(body);
+        callback(IAconfig.dataTurn);
+      }
+  });
+}
+
+function getRand(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+function move(b){
+  if(b.status == 1){
+    // C'est a moi (l'IA) de jouer
+    log.info("A moi de jouer... (Tour " + b.numTour)
+    if(b.numTour == 1){
+      // Ici on a pas le choix... On joue au centre
+      log.info("Tour 1: Je joue au centre...")
+      play(9,9);
+    }
+    if (b.numTour == 2){
+      var x = getRand(6,12);
+      var y = getRand(6,12);
+      log.info("Tour 2: Je joue dans le cadre du milieu en " + x + ";" + y + "...")
+      play(x,y);
+    }
+    if (b.numTour > 2){
+      var x = getRand(0,18);
+      var y = getRand(0,18);
+      log.info("Tour " + b.numTour + ": Je joue dans le cadre du milieu en " + x + ";" + y + "...")
+      play(x,y);
+    }
+  } else {
+    log.info("Attente...")
+  }
+}
+
 //////////////////////////
 
 if (init()) {
-    connect();
-    console.log(IAconfig.data);
-    //play();
+    connect(printInConsole);
+    if(!finPartie){
+      var intervalHolder = setInterval(function() {
+        turn(move)
+      },500);
+    } else {
+      console.log("fin")
+    }
 }
