6a6b641b33fb2581f7250c6c2f2c35cf27433d37
Corentin MOREAU <corentin.moreau2@gmail.com>
2017-04-12 21:32:14 +0200

    début IA

---
+++ ai/ai.js
@@ -0,0 +1,51 @@
+var log = require('fancy-log');
+var request = require('request');
+
+var IAconfig = {
+    adresseAPI: '',
+    nomEquipe: '',
+    data: ''
+};
+
+
+function init() {
+    if (process.argv[2] && process.argv[3]) {
+        IAconfig.adresseAPI = process.argv[2];
+        IAconfig.nomEquipe = process.argv[3];
+        return true;
+    } else {
+        process.on('exit', (code) => {
+            console.log('Usage: node ai.js <AdresseAPI> <NomEquipe>');
+            console.log('Exemple: node ai.js http://localhost:3000 Bordeaux');
+        });
+
+    }
+
+}
+
+function connect() {
+    log.info("Connexion de l'IA...")
+    reqConnexion();
+}
+
+function reqConnexion() {
+    request(IAconfig.adresseAPI + "/connect/" + IAconfig.nomEquipe, function(error, response, body) {
+        if (response && response.statusCode == 200) {
+            log.info("IA connectée avec succès !")
+            IAconfig.data = body;
+        } else {
+            process.on('exit', (code) => {
+                log.error('Erreur de connexion à l\'API');
+            });
+        }
+
+    });
+}
+
+//////////////////////////
+
+if (init()) {
+    connect();
+    console.log(IAconfig.data);
+    //play();
+}
