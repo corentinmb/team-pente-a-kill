218352bbfa1c24b43aa81f1fa4f1734a96dfce8d
Richie <afterprorichie@montoux-jb.fr>
2017-04-14 01:40:13 +0200

    Affichage whoPlays fini

--- routes/board.js
+++ routes/board.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
-  res.render('board', {id: req.params.id});
+  res.render('board', {id: req.params.id}, {name: game.player1.nomJoueur});
 });
 
 module.exports = router;
