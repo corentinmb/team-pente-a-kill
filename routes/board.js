25899f98d805a14dcbb6a7d64e35da372ecd7a35
Richie <afterprorichie@montoux-jb.fr>
2017-04-14 01:57:51 +0200

    Correction bug visuel

--- routes/board.js
+++ routes/board.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
-  res.render('board', {id: req.params.id}, {name: game.player1.nomJoueur});
+  res.render('board', {id: req.params.id});
 });
 
 module.exports = router;
